6b0b92cd413357ee6a0cbf1b549aac0fa114af40
Richard Rodriguez <github@justrodders.co.uk>
2017-11-28 22:48:16 +0000

    Add test case for Conjured items, which currently fails

---
+++ test/com/gildedrose/ConjuredItemsTestScenarios.java
@@ -0,0 +1,23 @@
+package com.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConjuredItemsTestScenarios {
+
+    @Test
+    public void ConjuredItems_quality_decreases_by_two_each_day() {
+        Item[] items = {
+                new Item("Conjured Mana Cake", 3, 6)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        app.updateQuality();
+
+        assertEquals(4, items[0].quality);
+        assertEquals(2, items[0].sellIn);
+    }
+
+}
